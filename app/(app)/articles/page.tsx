6660c5ffc642adfe66694d2f2e3b10db41e5eba8
Victoria Gavrishchuk <vt211_gvo@student.ztu.edu.ua>
2024-04-21 19:13:26 +0300

    feat: add article index page

--- app/(app)/articles/page.tsx
+++ app/(app)/articles/page.tsx
@@ -1,7 +1,14 @@
-'use client'
-function ArticlesPage() {
+async function ArticlesPage() {
+    const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const json = await data.json();
+
     return (
-        <div>Articles page</div>
+        <div>Articles page
+
+            <div>
+                {JSON.stringify(json)}
+            </div>
+        </div>
     )
 }
 export default ArticlesPage;
